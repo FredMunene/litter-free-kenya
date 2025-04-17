a629d33111b789a91ddf2982eb3c40252aef6318
fredmunene <munene2050gitonga@gmail.com>
2025-04-17 18:40:38 +0300

    update proprty--save-dev @types/leaflet

--- components/map-component.tsx
+++ components/map-component.tsx
@@ -51,12 +51,9 @@ export default function MapComponent({ onLocationSelect, selectedLocation }: Map
   // Fix Leaflet default icon issue
   useEffect(() => {
     // Make sure this only runs on the client
-    delete L.Icon.Default.prototype._getIconUrl
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-      iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-      shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-    })
+    L.Icon.Default.prototype.options.iconRetinaUrl = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png"
+    L.Icon.Default.prototype.options.iconUrl = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png"
+    L.Icon.Default.prototype.options.shadowUrl = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png"
   }, [])
 
   return (
